7028f044e5af7e23af8bc4b91337b3b980b79412
bobcermak <145228912+bobcermak@users.noreply.github.com>
2024-01-29 13:30:10 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
 
+let soucet: number
+let soucetvse: number
+let soucetvse2:number
+let kalibrace: boolean = false
+
+
+input.onButtonPressed(Button.A, function() {
+    kalibrace = true
+    for (let i = 0; i < 20; i++) {
+        console.log(input.lightLevel())
+        basic.pause(50)
+        soucetvse += input.lightLevel()
+    }
+
+    soucetvse2 = soucetvse / 20
+    music.playTone(400,500)
+
+    kalibrace = false
+})
+
+while (true) {
+    if(!kalibrace) {
+        if (input.lightLevel() < soucetvse2 ) {
+            music.playTone(400,500)
+        }
+    }
+    console.log(input.lightLevel())
+    basic.pause(100)
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+

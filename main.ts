4d1db5202f252f801066c9d58649e63670742700
bobcermak <145228912+bobcermak@users.noreply.github.com>
2024-01-29 15:54:17 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,28 +7,29 @@ let kalibrace: boolean = false
 
 input.onButtonPressed(Button.A, function() {
     kalibrace = true
-    for (let i = 0; i < 20; i++) {
-        console.log(input.lightLevel())
+    for (let i = 0; i <= 20; i++) {
+        let svetlo1: number = input.lightLevel()
         basic.pause(50)
-        soucetvse += input.lightLevel()
+        soucetvse += svetlo1
     }
 
     soucetvse2 = soucetvse / 20
     music.playTone(400,500)
 
-    kalibrace = false
 })
 
 while (true) {
-    if(!kalibrace) {
-        if (input.lightLevel() < soucetvse2 ) {
+    let svetlo2:number = input.lightLevel()
+    console.log(svetlo2)
+    basic.pause(50)
+    if(kalibrace) {
+        if (svetlo2 < soucetvse2 ) {
             music.playTone(400,500)
+            
         }
     }
-    console.log(input.lightLevel())
-    basic.pause(100)
-    
 
+    
 }
 
 
